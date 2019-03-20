558b1debc0ed64b3b2af0db1b7dbe7c02172a5e3
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 15:23:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-  
+  words.any? do |word|
+  word.length > 4
+end
 end
 
 def find_the_cheese# code an argument here
