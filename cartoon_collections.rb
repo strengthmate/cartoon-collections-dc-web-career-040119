f7966188cc861e3b698b866bd14a66ac11118089
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 14:37:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  arr = [1..dwarf_names.length]
+  
 end
 
 def summon_captain_planet# code an argument here
