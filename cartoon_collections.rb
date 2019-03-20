9a0de7491850ab0072127898b06a9cdb227a4dca
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 14:52:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,25 +5,7 @@ def roll_call_dwarves(dwarf_names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  
-  cool_nums = [1, 2, 3]
- 
-def change_nums(nums)
-  nums.collect do |x| 
-    x + 1
-  end
-end
- 
-change_nums(cool_nums)
-
-
-a = [ "a", "b", "c", "d" ]
-a.collect {|x| x + "!"}      
-a.map.with_index {|x, i| x * i}
-a       
-
-  
-  
+  planeteer_calls.collect {|x| x.upcase + "!"}  
 end
 
 def long_planeteer_calls# code an argument here
